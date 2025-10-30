fa923d919bff731d9449f5cb74c93e08be6c9c4d
unknown <zeeshanansari1232@gmail.com>
2025-10-30 22:44:40 +0530

    js-2

---
+++ javaScript/js-2/index.js
@@ -0,0 +1,43 @@
+// var age = 25;
+
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+// solve();
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// let a = 10;
+// a = "zeeshan"
+// a = true;
+// a = null;
+
+// const a = 28;
+// console.log(a);
+
+// const b = 20;
+// console.log(a);
+
+// let marks = 20;
+// console.log(marks);
+
+// marks = "zeeshan"
+// console.log(marks);
+
+// marks = true
+// console.log(marks);
+
+// let marks;
+// console.log(marks);
+
+let marks;
+console.log(typeof(marks));

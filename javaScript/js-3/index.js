64181f5797a5ec7b3067713bb8482049480c0f08
unknown <zeeshanansari1232@gmail.com>
2025-10-31 16:24:44 +0530

    js-4

---
+++ javaScript/js-3/index.js
@@ -0,0 +1,47 @@
+console.log("hello word");
+
+// let a = 10;
+// let b = 5;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// let a = 10;
+
+// a += 2;
+// console.log(a);
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// let age = 25;
+// let status = (age >18) ? 'i can vote ' : "i can not vote"
+// console.log(status);
+
+// let age = 25;
+// if(age > 18){
+//     console.log("you can vote");
+// } else {
+//     console.log("can not vote");
+// }
+
+let num = 3;
+switch(num){
+    case 1 : console.log("a");
+    break;
+    case 2 : console.log("d");
+    break;
+    case 3 : console.log("c");
+    break;
+    case 4 : console.log("q");
+    break;
+    default : console.log("L");
+}

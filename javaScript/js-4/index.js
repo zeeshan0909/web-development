eccdb278f0bf7e80c00b31ac30a62e0aadf9bff5
unknown <zeeshanansari1232@gmail.com>
2025-11-01 16:05:45 +0530

    js-4

---
+++ javaScript/js-4/index.js
@@ -0,0 +1,75 @@
+// console.log("hello world");
+// for(let i=1; i<=10; i++){
+//     console.log("hii")
+// }
+
+// for(let i=1; i<=5; i++){
+//     console.log(i);
+// }
+
+// for(let i=1; i<=6; i++){
+//     if(i == 4){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// for(let i=1; i<=6; i++){
+//     if(i == 4){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+// let i=1;
+// while(i<=10){
+//     console.log("jiii");
+//     i++;
+// }
+
+// let i =1;
+// do{
+//     console.log("hii");
+//     i++;
+// } while(i<=10);
+
+// let ss = "first string";
+// let pp = 'hello word'
+
+// let name = `zeeshan Ansari
+// i am form 
+// noorpur`;
+
+// console.log(name);
+
+// let fname = new String("Zeeshan Ansari");
+// console.log(fname);
+
+// let a = 'english';
+// let b = 'hindi'
+// let ans = a + b;
+// console.log(ans);
+
+// let a = 'english';
+// let b = 'hindi'
+
+// let ans = `${a}${b}` //this is the way when we use tactic symnol
+
+// let a = 'english';
+// let b = 'Hindi'
+
+// console.log(a.length);
+// console.log(a.toUpperCase());
+// console.log(b.toLocaleLowerCase());
+
+
+// let ss = "zeeshan";
+// console.log(ss.substring(2));
+// console.log(ss.substring(2,4));
+
+let ss = "this is the new chapter in my life";
+let words = ss.split(' ');
+console.log(words);
